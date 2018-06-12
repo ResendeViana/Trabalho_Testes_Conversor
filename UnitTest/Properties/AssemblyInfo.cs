06d12e2649dc86775c0786d4617f4ebcef7cd64d
Lucas de Resende Viana <lucasresende97@gmail.com>
2018-06-12 11:15:02 -0300

    Inclusão de Testes Unitários

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db797014-04ff-4160-8769-59a6442dccba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/TestesConversao.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trabalho_Testes_Conversor.BusinessLayer;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestesConversao
+    {
+        [TestMethod]
+        public void FtToM_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.FtToM(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void MToFt_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.MToFt(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SqydToM2_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.SqydToM2(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void M2ToSqyd_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.M2ToSqyd(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CuftToM3_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.CuftToM3(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void M3ToCuft_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.M3ToCuft(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void LbToKg_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.LbToKg(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void KgToLb_Test_Conversao()
+        {
+            try
+            {
+                double valor = 0.00;
+                double resultadoEsperado = 0.00;
+                double resultado = OperacoesMatematicas.KgToLb(valor);
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultadoEsperado, resultado);
+               
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
